a11292084db3975d6f2b3d88139f8ff02608cbee
Phillip Fleischer <pcfleischer@outlook.com>
2023-10-16 16:16:25 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,8 +17,8 @@ INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
 echo "Push to branch $INPUT_BRANCH";
-[ -z "${INPUT_GITHUB_TOKEN}" ] && {
-    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+[ -z "${INPUT_GITHUB-TOKEN}" ] && {
+    echo 'Missing input "github-token: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
 
@@ -40,7 +40,7 @@ fi
 
 cd "${INPUT_DIRECTORY}"
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB-TOKEN}@github.com/${REPOSITORY}.git"
 
 git config http.sslVerify false
 git config --local user.email "${INPUT_AUTHOR_EMAIL}"
